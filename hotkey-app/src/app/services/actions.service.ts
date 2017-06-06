20452e7f1e0323d4bdadfbf58fb92a3aa7b3bd51
Guillaume Lagrange <guillaume.lagrange@telecom-paristech.fr>
2017-06-06 21:54:47 +0200

    Forgot this

--- hotkey-app/src/app/services/actions.service.ts
+++ hotkey-app/src/app/services/actions.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
-import { Menu } from '../models/menu';
-import { MENUS } from '../models/mock-menus';
+import { Menu } from '../static/menu';
+import { MENUS } from '../static/mock-menus';
 
 @Injectable()
 export class ActionsService {
