e72a0f48705a97d75831b13671f65eb787c93029
Aloïs <alois.pourchot@gmail.com>
2017-06-20 00:27:05 +0200

    ajout du dataset + modification des menus

--- hotkey-app/src/app/static/mock-menus.ts
+++ hotkey-app/src/app/static/mock-menus.ts
@@ -2,6 +2,30 @@ import { Menu } from './menu';
 import { ACTIONS } from './mock-actions';
 
 export const MENUS: Menu[] = [
-  {name: "Menu1", actions: [ACTIONS[0], ACTIONS[2]]},
-  {name: "Menu2", actions: [ACTIONS[1], ACTIONS[3], ACTIONS[4]]}
+  	{name: "Animals", actions: [ACTIONS[0], ACTIONS[1], ACTIONS[2], 
+							    ACTIONS[3], ACTIONS[4], ACTIONS[5], 
+							    ACTIONS[6], ACTIONS[7], ACTIONS[8], 
+							    ACTIONS[9], ACTIONS[10], ACTIONS[11]]},
+
+  	{name: "Fruits", actions: [ACTIONS[12], ACTIONS[13], ACTIONS[14], 
+							   ACTIONS[15], ACTIONS[16], ACTIONS[17], 
+							   ACTIONS[18], ACTIONS[19], ACTIONS[20], 
+							   ACTIONS[21], ACTIONS[22]]},
+
+  	{name: "Vegetables", actions: [ACTIONS[23], ACTIONS[24], ACTIONS[25], 
+								   ACTIONS[26], ACTIONS[27], ACTIONS[28], 
+								   ACTIONS[29], ACTIONS[30], ACTIONS[31], 
+								   ACTIONS[32]]},
+
+    {name: "Clothing", actions: [ACTIONS[33], ACTIONS[34], ACTIONS[35], 
+							     ACTIONS[36], ACTIONS[37], ACTIONS[38], 
+							     ACTIONS[39], ACTIONS[40], ACTIONS[41]]},
+
+	{name: "Office", actions: [ACTIONS[42], ACTIONS[43], ACTIONS[44], 
+							     ACTIONS[45], ACTIONS[46], ACTIONS[47], 
+							     ACTIONS[48], ACTIONS[49], ACTIONS[50]]},
+
+	{name: "Recreation", actions: [ACTIONS[51], ACTIONS[52], ACTIONS[53], 
+							       ACTIONS[54], ACTIONS[55], ACTIONS[56], 
+							       	ACTIONS[57]]}
 ];
