c66521cc73e2e13d392538b0618cb42f297c4966
Xavier Mathery <xavier.mathery@telecom-paristech.fr>
2017-06-22 23:32:38 +0200

    New server with session

--- server/server.py
+++ server/server.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, request, make_response, jsonify, abort, send_file, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
-import os, base64, sys
+import os, base64, sys, json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -14,38 +14,48 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     ip = db.Column(db.String(45), nullable=False)
-    cookieId = db.Column(db.String(32), nullable=False)
+    cookieId = db.Column(db.String(32), nullable=False, unique=True)
+    sessions = db.relationship('Session', back_populates='user')
 
     def __init__(self, ip, cookieId):
         self.ip = ip
         self.cookieId = cookieId
 
     def __repr__(self):
-        return "User with ip {} and cookie {}".format(self.ip, self.cookieId)
+        return "User with ip {} and cookie {} with {} sessions".format(
+            self.ip, self.cookieId, len(self.sessions))
+
+class Session(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User', back_populates='sessions')
+    action = db.relationship('Action', back_populates='session')
+
+    def __init__(self, user):
+        self.user = user
+
 
 class Action(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    userId = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    user = db.relationship('User', uselist=False)
+    sessionId = db.Column(db.Integer, db.ForeignKey('session.id'), nullable=False)
+    session = db.relationship('Session', back_populates='action')
     time = db.Column(db.Integer, nullable=False)
     actionId = db.Column(db.Integer, nullable=False)
     correctAnswer = db.Column(db.Boolean, nullable=False)
-    hotKeyUsed = db.Column(db.Boolean, nullable=False)
-    errorCount = db.Column(db.Integer, nullable=False)
-    menuOpenened = db.Column(db.Boolean, nullable=False)
-    menuDelay = db.Column(db.Integer, nullable=False)
+    hotkeyUsed = db.Column(db.Boolean, nullable=False)
+    menuOpened = db.Column(db.Boolean, nullable=False)
+    menuDelay = db.Column(db.Integer, nullable=True)
 
-    def __init__(self, user, time, actionId, correctAnswer, hotKeyUsed, errorCount, menuOpenened, menuDelay):
-        self.user = user
+    def __init__(self, sessionId, time, actionId, correctAnswer, hotkeyUsed, menuOpened, menuDelay):
+        self.sessionId = sessionId
         self.time = time
         self.actionId = actionId
         self.correctAnswer = correctAnswer
         self.correctAnswer = correctAnswer
         self.correctAnswer = correctAnswer
         self.correctAnswer = correctAnswer
-        self.hotKeyUsed = hotKeyUsed
-        self.errorCount = errorCount
-        self.menuOpenened = menuOpenened
+        self.hotkeyUsed = hotkeyUsed
+        self.menuOpened = menuOpened
         self.menuDelay = menuDelay
 
     def __repr__(self):
@@ -54,7 +64,7 @@ class Action(db.Model):
 @app.route("/")
 def spa():
     def create_session():
-        newCookie = base64.b64encode(os.urandom(24))
+        newCookie = base64.b64encode(os.urandom(24)).decode("utf-8")
         ip = request.environ['REMOTE_ADDR']
         newUser = User(ip, newCookie)
         db.session.add(newUser)
@@ -70,7 +80,7 @@ def spa():
         newCookie = create_session()
         response.set_cookie("id", newCookie)
     else:
-        user = User.query.filter_by(cookieId=cookie).first()
+        user = User.query.filter(User.cookieId == cookie).one_or_none()
         if user is None:
             response.set_cookie("id", create_session())
     return response
@@ -78,18 +88,33 @@ def spa():
 @app.route("/log/action", methods=['POST'])
 def action():
     print("got action")
-    cookieId = request.cookies.get("id")
-    if cookieId is None:
-        abort(401)
     data = request.get_json(force=True)
-    user = User.query.filter_by(cookieId=cookieId).first()
-    if user is None:
-        abort(401)
-    newAction = Action(user, data.result.time, data.result.actionId, data.result.correctAnswer,
-                       data.result.hotKeyUsed, data.result.errorCount, data.result.menuOpenened, data.result.menuDelay)
+    result = data["result"]
+    newAction = Action(result["sessionId"], result["time"], result["actionId"],
+                       result["correctAnswer"], result["hotkeyUsed"],
+                       result["menuOpened"],
+                       result["menuDelay"] if "menuDelay" in result else None
+                       )
     db.session.add(newAction)
     db.session.commit()
     return "{}"
+
+@app.route("/log/session", methods=['POST'])
+def session():
+    cookieId = request.cookies.get("id")
+    print(cookieId)
+    if cookieId is None:
+        abort(401)
+    user = User.query.filter(User.cookieId == cookieId).one_or_none()
+    if user is None:
+        abort(401)
+    session = Session(user)
+    db.session.add(session)
+    db.session.commit()
+    print(session.id)
+    result = { "id": session.id }
+    return json.dumps(result)
+
 @app.route('/<path:path>')
 def send_js(path):
     return send_from_directory('../hotkey-app/dist/', path)
